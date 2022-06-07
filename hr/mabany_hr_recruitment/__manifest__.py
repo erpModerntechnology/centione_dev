8ede7cdf7e3fb1cfc845707a1767b4d4ddabc245
Abo El Fadl <fodaseken1997@gmail.com>
2022-06-07 22:06:39 +0200

    updated

---
+++ hr/mabany_hr_recruitment/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mabany Hr Recruitment",
+
+    'summary': """
+    Module for recuritment customizations
+        """,
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr_recruitment','hr'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_applicant_inherit.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
