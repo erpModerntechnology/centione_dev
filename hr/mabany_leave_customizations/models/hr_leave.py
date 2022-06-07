65cbcad09151db42c2d3a6843295049531bd63f3
Abo El Fadl <fodaseken1997@gmail.com>
2022-06-07 18:49:22 +0200

    updated

--- hr/mabany_leave_customizations/models/hr_leave.py
+++ hr/mabany_leave_customizations/models/hr_leave.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from datetime import timedelta
 
-from odoo import models, fields, api,_
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-
 
 
 class HrLeaveType(models.Model):
     _inherit = 'hr.leave.type'
 
-    holiday_type = fields.Selection([('marriage','جواز'),('casual','عارضة'),('sick','مرضى')])
+    holiday_type = fields.Selection(
+        [('marriage', 'جواز'), ('casual', 'عارضة'), ('sick', 'مرضى'), ('unpaid', 'غير مدفوع')])
     exception_constraint = fields.Boolean('Exception Constraint')
 
 
@@ -22,30 +22,16 @@ class HrLeave(models.Model):
     def constraint_number_of_days_casual(self):
         total_dur_casual = []
         total_dur_marriage = []
-        leave = self.env['hr.leave'].search([])
-        for rec in leave:
-            if self.employee_id.id == rec.employee_id.id:
-                if self.holiday_status_id.holiday_type == 'casual':
-                    if rec.exception_constraint == False:
-                        if rec.request_date_from.month == self.request_date_to.month:
-                            total_dur_casual.append(rec.number_of_days)
-                if rec.holiday_status_id.holiday_type == 'marriage':
-                        total_dur_marriage.append(rec.number_of_days)
-                        if sum(total_dur_marriage) > 7:
-                            raise ValidationError(_("Marriage Holiday must not exceeds a week maximum"))
-                        # if rec.request_date_from.month != rec.request_date_to.month:
-                        #         raise ValidationError(_("Casual Holiday must not be in 2 separate months"))
-        if sum(total_dur_casual) > 2:
-            raise ValidationError(_("Casual Holiday must not exceeds 2 days per month"))
-
-
-
-    # @api.constrains('number_of_days')
-    # def constraint_number_of_days_marriage(self):
-    #     leave = self.env['hr.leave'].search([])
-    #     total_dur = []
-    #     for rec in leave:
-    #         if self.employee_id.id == rec.employee_id.id:
-
-
-    
+        leave_casual = self.env['hr.leave'].search(
+            [('employee_id', '=', self.employee_id.id), ('holiday_status_id.holiday_type', '=', 'casual')])
+        leave_marriage = self.env['hr.leave'].search(
+            [('employee_id', '=', self.employee_id.id), ('holiday_status_id.holiday_type', '=', 'marriage')])
+        for lec in leave_casual:
+            if lec.exception_constraint == False:
+                total_dur_casual.append(lec.number_of_days)
+                if sum(total_dur_casual) > 2:
+                    raise ValidationError(_("Casual Holiday must not exceeds 2 days per month"))
+        for lem in leave_marriage:
+            total_dur_marriage.append(lem.number_of_days)
+            if sum(total_dur_marriage) > 7:
+                raise ValidationError(_("Marriage Holiday must not exceeds a week maximum"))
--- hr/mabany_salary_rules/models/hr_employee.py
+++ hr/mabany_salary_rules/models/hr_employee.py
@@ -6,18 +6,28 @@ from odoo import models, fields, api
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-
+    def get_unpaid_day(self, payslip, contract):
+        total_deduct = []
+        emp = self.env['hr.leave'].search(
+            [('employee_ids', '=', self.id), ('request_date_from', '>=', payslip.date_from),
+             ('request_date_from', '<=', payslip.date_to)])
+        for rec in emp:
+            if rec.holiday_status_id.holiday_type == 'unpaid':
+                total_wage_per_day = contract.wage / 30
+                total_deduct.append(total_wage_per_day)
+        total_wage_deduct = sum(total_deduct)
+        return total_wage_deduct
 
     def sick_leave_deduct(self):
         total_dur = []
-        sick = self.env['hr.leave'].search([('employee_ids','=',self.id)])
+        sick = self.env['hr.leave'].search([('employee_ids', '=', self.id)])
         if sick:
             for rec in sick:
-                    if rec.holiday_status_id.holiday_type == 'sick':
-                            date = abs(rec.request_date_from - rec.request_date_to)
-                            diff = date.days
-                            print(date)
-                            total_dur.append(diff)
+                if rec.holiday_status_id.holiday_type == 'sick':
+                    date = abs(rec.request_date_from - rec.request_date_to)
+                    diff = date.days
+                    print(date)
+                    total_dur.append(diff)
             if 90 > sum(total_dur):
                 if sum(total_dur) < 30:
                     return 0
@@ -30,16 +40,12 @@ class HrEmployee(models.Model):
                 total_deduct = self.contract_id.wage * 0.15
                 return total_deduct
 
-
-    def get_sick_leave(self,payslip):
-        emp = self.env['hr.leave'].search([('employee_ids', '=', self.id),('request_date_from','>=',payslip.date_from),
-                                           ('request_date_from','<=',payslip.date_to)])
+    def get_sick_leave(self, payslip):
+        emp = self.env['hr.leave'].search(
+            [('employee_ids', '=', self.id), ('request_date_from', '>=', payslip.date_from),
+             ('request_date_from', '<=', payslip.date_to)])
         if emp:
             total_deduct_sick = self.sick_leave_deduct()
             return total_deduct_sick
         else:
             return 0
-
-
-
-
