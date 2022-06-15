e364428f2f0e71cb7cc5fb91a690dc6220c95a87
Mahmoud ALaa <m.alaa@centione.com>
2022-06-15 13:46:03 +0200

    upda

---
+++ thrd_party/microsoft_sso_integration/controllers/main.py
@@ -0,0 +1,135 @@
+import functools
+import logging
+
+import json
+
+import base64
+from odoo.tools.translate import _
+from werkzeug.utils import redirect
+import odoo
+import werkzeug.urls
+import werkzeug.utils
+from werkzeug.exceptions import BadRequest
+import time
+from odoo import api, http, SUPERUSER_ID, _, registry
+from odoo.exceptions import AccessDenied
+from odoo.http import request
+from odoo import registry as registry_get
+
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome as Home
+from odoo.addons.web.controllers.main import db_monodb, ensure_db, set_cookie_and_redirect, login_and_redirect
+
+
+_logger = logging.getLogger(__name__)
+
+#----------------------------------------------------------
+# helpers
+#----------------------------------------------------------
+def fragment_to_query_string(func):
+    @functools.wraps(func)
+    def wrapper(self, *a, **kw):
+        kw.pop('debug', False)
+        if not kw:
+            return """<html><head><script>
+                var l = window.location;
+                var q = l.hash.substring(1);
+                var r = l.pathname + l.search;
+                if(q.length !== 0) {
+                    var s = l.search ? (l.search === '?' ? '' : '&') : '?';
+                    r = l.pathname + l.search + s + q;
+                }
+                if (r == l.pathname) {
+                    r = '/';
+                }
+                window.location = r;
+            </script></head><body></body></html>"""
+        return func(self, *a, **kw)
+    return wrapper
+
+
+class OAuthLogin(Home):
+    def list_providers(self):
+        try:
+            providers = request.env['auth.oauth.provider'].sudo().search_read([('enabled', '=', True)])
+        except Exception:
+            providers = []
+        for provider in providers:
+            redirect_url = request.httprequest.url_root + 'microsoft_sso_integration/microsoft'
+            return_url = request.httprequest.url_root + 'auth_oauth/signin'
+
+            state = self.get_state(provider)
+            if provider['name'] == 'Microsoft OAuth2':
+                params = dict(
+                    response_type='code',
+                    client_id=provider['client_id'],
+                    redirect_uri=redirect_url,
+                    scope=provider['scope'],
+                    state=json.dumps(state),
+                )    
+            else:   
+                params = dict(
+                    response_type='token',
+                    client_id=provider['client_id'],
+                    redirect_uri=return_url,
+                    scope=provider['scope'],
+                    state=json.dumps(state),
+                )
+            provider['auth_link'] = "%s?%s" % (provider['auth_endpoint'], werkzeug.urls.url_encode(params))
+        return providers
+
+
+
+class MicrosoftRoute(http.Controller):
+
+
+    @http.route('/microsoft_sso_integration/microsoft', type='http', auth='none')
+    @fragment_to_query_string    
+    def azure_return(self, **kw):
+        state = json.loads(kw['state'])
+        dbname = state['d']
+        if not http.db_filter([dbname]):
+            return BadRequest()
+        provider = state['p']
+        context = state.get('c', {})
+        registry = registry_get(dbname)
+        with registry.cursor() as cr:
+            try:
+                env = api.Environment(cr, SUPERUSER_ID, context)
+                credentials = env['res.users'].sudo().auth_oauth_microsoft(provider, kw)
+                cr.commit()
+                action = state.get('a')
+                menu = state.get('m')
+                redirect = werkzeug.urls.url_unquote_plus(state['r']) if state.get('r') else False
+                url = '/web'
+                if redirect:
+                    url = redirect
+                elif action:
+                    url = '/web#action=%s' % action
+                elif menu:
+                    url = '/web#menu_id=%s' % menu
+
+                resp = login_and_redirect(*credentials, redirect_url=url)
+                # Since /web is hardcoded, verify user has right to land on it
+                if werkzeug.urls.url_parse(resp.location).path == '/web' and not request.env.user.has_group('base.group_user'):
+                    resp.location = '/'
+                return resp
+            except AttributeError:
+                # auth_signup is not installed
+                _logger.error("auth_signup not installed on database %s: oauth sign up cancelled." % (dbname,))
+                url = "/web/login?oauth_error=1"
+            except AccessDenied:
+                # oauth credentials not valid, user could be on a temporary session
+                _logger.info('OAuth2: access denied, redirect to main page in case a valid session exists, without setting cookies')
+                url = "/web/login?oauth_error=3"
+                redirect = werkzeug.utils.redirect(url, 303)
+                redirect.autocorrect_location_header = False
+                return redirect
+            except Exception as e:
+                # signup error
+                _logger.exception("OAuth2: %s" % str(e))
+                url = "/web/login?oauth_error=2"
+
+        return set_cookie_and_redirect(url)
+
+
+
