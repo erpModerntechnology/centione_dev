063c0118ef115e2d224145f8ebf40176385f4556
Mahmoud ALaa <m.alaa@centione.com>
2022-06-27 00:22:59 +0200

    upda

---
+++ hr/mabany_portal_employee_profile/models/hr_employee_inherit.py
@@ -0,0 +1,30 @@
+# # -*- coding: utf-8 -*-
+from odoo import models, fields, api , _
+
+class hr_employee_inherit(models.Model):
+    _inherit = "hr.employee"
+
+    def create_portal_user(self):
+        #for not set user_id ,create portal user
+        for rec in self:
+            if not rec.user_id:
+                created_user=self.env['res.users'].create({
+                    'name':rec.name,
+                    'email':rec.work_email,
+                    'login':rec.work_email,
+                    'password':'123g',
+                })
+                portal_user_group = rec.env.ref('base.group_portal')
+                public_user_group = rec.env.ref('base.group_public')
+                internal_user_group = rec.env.ref('base.group_user')
+
+
+
+                public_user_group.users = [(3, created_user.id)]
+                internal_user_group.users = [(3, created_user.id)]
+
+                portal_user_group.users = [(4, created_user.id)]
+
+                rec.user_id=created_user.id
+
+
