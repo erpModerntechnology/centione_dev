052d65f79df5a57fc4ba2a9f37744528e5b53d95
Mahmoud ALaa <m.alaa@centione.com>
2022-06-27 14:17:12 +0200

    upda

--- hr/mabany_hr_self_service/models/hr_excuse.py
+++ hr/mabany_hr_self_service/models/hr_excuse.py
@@ -8,42 +8,23 @@ import calendar
 
 class HrExcuse(models.Model):
     _name = 'hr.excuse'
-    # _inherit = 'hr.self.service'
     _inherit = ['hr.self.service', 'mail.thread', 'mail.activity.mixin']
 
     start_date = fields.Datetime()
     end_date = fields.Datetime()
     period = fields.Float(compute='_compute_period', store=True)
 
-    # @api.constrains('period')
-    # def _check_period(self):
-    #     max_period = self.employee_id.max_excuse_period
-    #     if self.period > max_period:
-    #         raise UserError(_('Period exceeds employee\'s allowed period.'))
-    #     month = self.start_date.month
-    #     year = self.start_date.year
-    #     month_start = datetime(day=1, month=month, year=year, hour=0, minute=0, second=0)
-    #     month_end = datetime(day=calendar.monthrange(year, month)[1], month=month, year=year, hour=23, minute=59, second=59)
-    #     max_month_request = self.employee_id.number_excuse_per_month
-    #     excuses_this_month = self.env['hr.excuse'].search([('employee_id', '=', self.employee_id.id),
-    #                                                        ('start_date', '>=', month_start),
-    #                                                        ('end_date', '<=', month_end),
-    #                                                        ('state', '=', 'draft')])
-    #     if len(excuses_this_month) > max_month_request:
-    #         raise UserError(_('Period exceeds employee\'s allowed requests per month.'))
+    my_manager = fields.Boolean(string='Current user is my_manager',
+                                              compute='_compute_my_manager')
 
 
-    # @api.constrains('start_date', 'end_date')
-    # def _check_date(self):
-    #     max_period = self.employee_id.max_excuse_period
-    #     excuses_this_month = self.env['hr.excuse'].search([('employee_id', '=', self.employee_id.id),('state','=','validate')])
-    #     # if len(excuses_this_month) > 1:
-    #     #     raise UserError(_('this excuse is already existed'))
-    #     total = 0
-    #     for rec in excuses_this_month:
-    #         total += rec.period
-    #         if total > max_period:
-    #             raise UserError(_('Period exceeds employee\'s allowed period .'))
+    #button will appear for manager of the employee
+    def _compute_my_manager(self):
+        if self.sudo().employee_id.parent_id.user_id.id == self.env.user.id:
+            self.my_manager = True
+        else:
+            self.my_manager = False
+
 
     @api.depends('start_date', 'end_date')
     def _compute_period(self):
@@ -82,5 +63,20 @@ class HrExcuse(models.Model):
             'date_to': self.end_date
         })
 
+
+    def first_approve(self):
+        for rec in self:
+            if rec.filtered(lambda holiday: holiday.state != 'draft'):
+                raise UserError(_('Excuse request must be in Draft state in order to Approve it.'))
+            rec.write({'state': 'approve'})
+
+
+
+    def second_approve(self):
+        for rec in self:
+            if rec.filtered(lambda holiday: holiday.state != 'approve'):
+                raise UserError(_('Excuse request must be in Approved state in order to validate it.'))
+
+            rec.validate()
 
 
