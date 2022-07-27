0f4987a3734bfe9ae2f2af77b93b221d7530e710
Mahmoud ALaa <m.alaa@centione.com>
2022-07-27 13:57:29 +0200

    remove denpending on website

--- hr/mabany_portal_leave/__manifest__.py
+++ hr/mabany_portal_leave/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr_holidays','web','portal','website','hr',],
+    'depends': ['base','hr_holidays','web','portal','hr',],
 
     # always loaded
     'data': [
