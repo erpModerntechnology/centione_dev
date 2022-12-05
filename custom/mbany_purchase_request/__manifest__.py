d5c9516cb38d10e343d22a6ec239620172b15094
kerolesayed <kerolesayed@gmail.com>
2022-12-05 11:53:19 +0200

    edit

--- custom/mbany_purchase_request/__manifest__.py
+++ custom/mbany_purchase_request/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account_budget','product','account','mail'],
+    'depends': ['base','account_budget','product','account','mail','purchase'],
 
     # always loaded
     'data': [
